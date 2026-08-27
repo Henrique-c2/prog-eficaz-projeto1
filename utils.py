3b3c1f7d544117bad6f3623bca52269f2b71648e
Henrique-c2 <hibc22@gmail.com>
2026-08-27 08:37:42 -0300

    Fim do handout

---
+++ utils.py
@@ -0,0 +1,25 @@
+
+import json
+
+def load_data(nome_arquivo):
+    with open(f"static/data/{nome_arquivo}", "r") as arquivo_json:
+        texto = arquivo_json.read()
+
+        dicionario = json.loads(texto)
+        return dicionario
+
+    
+def load_template (nome_arquivo):
+    with open(f"static/templates/{nome_arquivo}","r") as template:
+        texto = template.read()
+
+        return texto
+
+def add_form (titulo,detalhes):
+    with open(f"static/data/notes.json","r", encoding="utf-8") as arquivo_json:
+        dicionario = json.load(arquivo_json)
+
+    with open(f"static/data/notes.json","w", encoding="utf-8") as arquivo_json:
+        dicionario.append({"titulo" : titulo, "detalhes" : detalhes})
+
+        dicionario = json.dump(dicionario, arquivo_json)
